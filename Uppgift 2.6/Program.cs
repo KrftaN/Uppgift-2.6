f6ac369200576f5c98a000f67eac4233f01a0bb8
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-15 13:26:31 +0200

    Add project files.

---
+++ Uppgift 2.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Uppgift_2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening:");
+            char[] punkt = { '.' };
+            string input = Console.ReadLine().TrimEnd(punkt);
+
+            Console.WriteLine(string.Join(" ", input.Split(' ').Reverse()) + ".");
+            Console.ReadKey();
+        }
+    }
+}
